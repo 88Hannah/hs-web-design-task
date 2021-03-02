624fafff2fe7845a6dc84bde094df0305037cc7e
88Hannah <hannahsangar@gmail.com>
2021-03-02 18:26:34 +0000

    Carousel working for all screen sizes. Padding and Margins also adjusted accordingly.

--- src/components/Wines.js
+++ src/components/Wines.js
@@ -8,20 +8,6 @@ function Wines() {
     const wineCards = winesData.wines.map(wine => (
         <WineCard key={wine.id} wineData={wine} />
     ));
-    
-
-    const minWidth768 = window.matchMedia('(min-width: 100px)');
-    let carousel
-
-    if (minWidth768.matches) {
-        carousel =  <div className="wine-cards main-carousel" data-flickity='{"contain" : true, "cellAlign" : "left" }'>
-                        {wineCards}
-                    </div>
-    } else {
-        carousel =  <div className="wine-cards main-carousel" data-flickity='{}'>
-                        {wineCards}
-                    </div>
-    }
 
     return (
 
@@ -31,7 +17,9 @@ function Wines() {
 
                 <h2 className="wines__title">Meet the 12 wines in your mixed case</h2>
 
-                {carousel}
+                <div className="wine-cards main-carousel" data-flickity='{"contain" : true, "cellAlign" : "left" }'>
+                    {wineCards}
+                </div>
 
             </div>
 
